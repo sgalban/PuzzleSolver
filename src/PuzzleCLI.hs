7cd319f3cd0528fefde914f75b5fbd2f90451d03
Steven Galban <stgalban1@gmail.com>
2024-11-24 13:52:55 -0500

    Flesh out PuzzleCLI some more
    Also made some small adjustments to the proposal

--- src/PuzzleCLI.hs
+++ src/PuzzleCLI.hs
@@ -1,5 +1,7 @@
 module PuzzleCLI where
 import Puzzle (Puzzle, PuzzleSolution (puzzle))
+import Data.Maybe (fromMaybe)
+import Data.List qualified as List
 
 data Looper = Looper
   { filename :: Maybe String,
@@ -16,4 +18,29 @@ looper = go initialLooper
   where
     go :: Looper -> IO ()
     go l = do
-      undefined
+      putStr (fromMaybe "" (filename l) ++ "> ")
+      str <- getLine
+      case List.uncons (words str) of
+        -- | Load a new puzzle file and reset the looper
+        Just (":l", [fn]) -> loadFile fn l
+        -- | Validate the current solution
+        Just (":v", _) -> undefined
+        -- | Reset the current solution, but not the underlying puzzle
+        Just (":r", _) -> undefined
+        -- | Attempt to solve the current puzzle, starting with the current solution
+        Just (":s", _) -> undefined
+        -- | Print out the current puzzle as an ASCII grid
+        Just (":p", [arg]) -> undefined
+        -- | Add a value to the current solution
+        Just (":a", args) -> undefined
+        -- | Delete a value from the current solution
+        Just (":d", args) -> undefined
+        -- | Quit the looper
+        Just (":q", _) -> return ()
+        _ -> do
+          putStrLn ("Unknown command \"" <> str <> "\"")
+          go l
+
+-- | Load a new puzzle file and reset the looper
+loadFile :: String -> Looper -> IO ()
+loadFile fn l = undefined
