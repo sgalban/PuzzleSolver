b2b8cfc7a217286fd0cc3748e64d5fa32f65edec
Steven Galban <stgalban1@gmail.com>
2024-11-24 00:30:12 -0500

    Add PuzzleSolver properties
    Also implemented Arbitrary for most model classes.
    Shrink has not yet been implemented

--- src/Puzzle.hs
+++ src/Puzzle.hs
@@ -1,5 +1,8 @@
 module Puzzle where
 import Data.Map ( Map )
+import Test.QuickCheck (Arbitrary)
+import qualified Test.QuickCheck as QC
+import Control.Monad (liftM2, liftM3)
 
 data NumExp
   = Number Int
@@ -14,9 +17,9 @@ data Bop
   | Divide
   | Modulo
   | Power
-  deriving (Show, Eq)
+  deriving (Show, Eq, Enum, Bounded)
 
-newtype Range = Range (NumExp, NumExp) deriving(Show, Eq)
+newtype Range = Range (NumExp, NumExp) deriving (Show, Eq)
 
 data Puzzle = Grid {
   width :: Int,
@@ -37,7 +40,7 @@ data ConstrainedCells = ConstrainedCells {
 
 data Constraint
   = PC PrimitiveConstraint
-  | ConstraintList [Constraint]
+  | ConstraintList [PrimitiveConstraint]
   deriving (Show, Eq)
 
 instance Semigroup Constraint where
@@ -47,9 +50,9 @@ instance Semigroup Constraint where
   n@(PC Never) <> _ = n
   _ <> n@(PC Never) = n
   ConstraintList l1 <> ConstraintList l2 = ConstraintList $ l1 ++ l2
-  ConstraintList l <> c = ConstraintList $ c : l
-  c <> ConstraintList l = ConstraintList $ c : l
-  c1 <> c2 = ConstraintList [c1, c2]
+  ConstraintList l <> (PC p) = ConstraintList $ p : l
+  (PC p) <> ConstraintList l = ConstraintList $ p : l
+  (PC p1) <> (PC p2) = ConstraintList [p1, p2]
 
 instance Monoid Constraint where
   mempty :: Constraint
@@ -91,7 +94,97 @@ instance Monoid CellGroup where
   mempty :: CellGroup
   mempty = CellList []
 
+isValidPuzzle :: Puzzle -> Bool
+isValidPuzzle = undefined
+
 data PuzzleSolution = PuzzleSolution {
   puzzle :: Puzzle,
   cellValues :: Map (Int, Int) Int
   } deriving (Show, Eq)
+
+-- QuickCheck instances
+
+instance Arbitrary Bop where
+  arbitrary :: QC.Gen Bop
+  arbitrary = QC.arbitraryBoundedEnum
+
+instance Arbitrary NumExp where
+  arbitrary :: QC.Gen NumExp
+  arbitrary = QC.sized genExp
+    where
+      genExp 0 = QC.frequency [
+        (1, return $ RepeatVar 0),
+        (1, return $ RepeatVar 1),
+        (2, Number <$> QC.arbitrary)]
+      genExp n = QC.frequency [
+        (2, genExp 0),
+        (n, Op2 <$> genExp n' <*> QC.arbitrary <*> genExp n')]
+          where
+            n' = n `div` 2
+
+instance Arbitrary Range where
+  arbitrary :: QC.Gen Range
+  arbitrary = liftM2 (curry Range) QC.arbitrary QC.arbitrary
+
+instance Arbitrary PrimitiveConstraint where
+  arbitrary :: QC.Gen PrimitiveConstraint
+  arbitrary = QC.oneof [
+    Unique <$> QC.arbitrary,
+    AddsTo <$> QC.arbitrary,
+    MultsTo <$> QC.arbitrary,
+    GreaterThan <$> QC.arbitrary <*> QC.arbitrary,
+    LessThan <$> QC.arbitrary <*> QC.arbitrary,
+    return Always,
+    return Never]
+
+instance Arbitrary Constraint where
+  arbitrary :: QC.Gen Constraint
+  arbitrary = QC.sized genCon
+    where
+      genCon 0 = PC <$> QC.arbitrary
+      genCon n = QC.oneof [
+        genCon 0,
+        ConstraintList <$> QC.listOf QC.arbitrary]
+
+instance Arbitrary CellGroup where
+  arbitrary :: QC.Gen CellGroup
+  arbitrary = QC.oneof [
+    ACell <$> QC.arbitrary <*> QC.arbitrary,
+    CellList <$> QC.listOf QC.arbitrary,
+    Subgrid <$> QC.arbitrary <*> QC.arbitrary <*> QC.arbitrary <*> QC.arbitrary,
+    Row <$> QC.arbitrary <*> rowColBoundGen,
+    Col <$> QC.arbitrary <*> rowColBoundGen,
+    Inverse <$> QC.arbitrary,
+    return All,
+    return Unconstrained]
+    where
+      rowColBoundGen :: QC.Gen (Maybe (NumExp, NumExp))
+      rowColBoundGen = QC.oneof [
+        Just <$> liftM2 (,) QC.arbitrary QC.arbitrary,
+        return Nothing]
+
+instance Arbitrary ConstrainedCells where
+  arbitrary :: QC.Gen ConstrainedCells
+  arbitrary = liftM2 ConstrainedCells QC.arbitrary QC.arbitrary
+
+instance Arbitrary ConstraintRule where
+  arbitrary :: QC.Gen ConstraintRule
+  arbitrary = QC.sized genConRule
+    where
+      genConRule 0 = QC.oneof [
+        CellInit <$> QC.arbitrary <*> QC.arbitrary,
+        CC <$> QC.arbitrary]
+      genConRule n = QC.frequency [
+        (2, genConRule 0),
+        (1, Repeat <$> QC.arbitrary <*> genConRule (n `div` 2))]
+
+instance Arbitrary Puzzle where
+  arbitrary :: QC.Gen Puzzle
+  arbitrary = liftM3 Grid genDim genDim $ QC.listOf QC.arbitrary
+    where
+      genDim :: QC.Gen Int
+      genDim = QC.suchThat QC.arbitrary (\x -> x > 1 && x < 10)
+
+instance Arbitrary PuzzleSolution where
+  arbitrary :: QC.Gen PuzzleSolution
+  arbitrary = undefined
--- src/PuzzleSolver.hs
+++ src/PuzzleSolver.hs
@@ -1,1 +1,62 @@
-module PuzzleSolver where
+module PuzzleSolver(solve, validate) where
+import Puzzle
+import qualified Test.QuickCheck as QC
+import Data.Maybe (isJust)
+import qualified Data.List as List
+import qualified Data.Map as Map
+
+solve :: Puzzle -> Maybe PuzzleSolution
+solve puzzle = undefined
+
+validate :: PuzzleSolution -> Bool
+validate ps = undefined
+
+prop_solveSamePuzzle :: Puzzle -> QC.Property
+prop_solveSamePuzzle p = isJust ps QC.==> maybe True (\ps' -> puzzle ps' == p) ps
+  where
+    ps = solve p
+
+prop_solveValid :: Puzzle -> QC.Property
+prop_solveValid p = isJust ps QC.==> maybe True validate ps
+  where
+    ps = solve p
+
+prop_solveComplete :: Puzzle -> QC.Property
+prop_solveComplete p@(Grid w h _) = isJust ps QC.==> maybe True complete ps
+  where
+    ps = solve p
+    coords = List.sort [(r, c) | r <- [0..h-1], c <- [0..w-1]]
+    complete :: PuzzleSolution -> Bool
+    complete (PuzzleSolution _ s) = List.sort (Map.keys s) == coords
+
+prop_emptyValid :: Puzzle -> QC.Property
+prop_emptyValid p = isValidPuzzle p QC.==> validate $ PuzzleSolution p Map.empty
+
+prop_subsolutionValid :: PuzzleSolution -> QC.Property
+prop_subsolutionValid ps@(PuzzleSolution p s) =
+  validate ps QC.==> case Map.keys s of
+    (k : _) ->  validate $ PuzzleSolution p (Map.delete k s)
+    _ -> True
+
+prop_addInvalid :: PuzzleSolution -> Int -> Int -> Int -> QC.Property
+prop_addInvalid ps@(PuzzleSolution p@(Grid w h _) s) r c v =
+  not (validate ps) QC.==> case Map.keys s of
+    (k : _) ->  not . validate $ PuzzleSolution p (Map.insert pair v s)
+    _ -> True
+    where
+      pair = (r `mod` h, c `mod` h)
+
+checkProps :: IO ()
+checkProps = do
+  putStrLn "solveSamePuzzle"
+  QC.quickCheck prop_solveSamePuzzle
+  putStrLn "solveValid"
+  QC.quickCheck prop_solveValid
+  putStrLn "solveComplete"
+  QC.quickCheck prop_solveComplete
+  putStrLn "emptyValid"
+  QC.quickCheck prop_emptyValid
+  putStrLn "subsolutionValid"
+  QC.quickCheck prop_subsolutionValid
+  putStrLn "addInvalid"
+  QC.quickCheck prop_addInvalid
